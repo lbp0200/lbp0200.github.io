23432cc772830e275c61fe3300ecddb1e050cee5
刘博平(焦点) <bopingliu@sohu-inc.com>
2017-01-12 15:21:17 +0800

    Site updated: 2017-01-12 15:21:17

--- js/script.js
+++ js/script.js
@@ -1,85 +1,121 @@
-(function($){
-
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
+function createCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
     }
 
-    $('.article-share-box.on').hide();
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
 
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
 
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('image-link')) return;
+(function ($) {
 
-      var alt = this.alt;
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+    // Share
+    $('body').on('click', function () {
+        $('.article-share-box.on').removeClass('on');
+    }).on('click', '.article-share-link', function (e) {
+        e.stopPropagation();
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="image-link"></a>');
+        var $this = $(this),
+            url = $this.attr('data-url'),
+            encodedUrl = encodeURIComponent(url),
+            id = 'article-share-box-' + $this.attr('data-id'),
+            offset = $this.offset();
+
+        if ($('#' + id).length) {
+            var box = $('#' + id);
+
+            if (box.hasClass('on')) {
+                box.removeClass('on');
+                return;
+            }
+        } else {
+            var html = [
+                '<div id="' + id + '" class="article-share-box">',
+                '<input class="article-share-input" value="' + url + '">',
+                '<div class="article-share-links">',
+                '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+                '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+                '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+                '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+                '</div>',
+                '</div>'
+            ].join('');
+
+            var box = $(html);
+
+            $('body').append(box);
+        }
+
+        $('.article-share-box.on').hide();
+
+        box.css({
+            top: offset.top + 25,
+            left: offset.left
+        }).addClass('on');
+    }).on('click', '.article-share-box', function (e) {
+        e.stopPropagation();
+    }).on('click', '.article-share-box-input', function () {
+        $(this).select();
+    }).on('click', '.article-share-box-link', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
     });
 
-    $(this).find('.image-link').each(function(){
-      $(this).attr('rel', 'article' + i);
+    // Caption
+    $('.article-entry').each(function (i) {
+        $(this).find('img').each(function () {
+            if ($(this).parent().hasClass('image-link')) return;
+
+            var alt = this.alt;
+            if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+
+            $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="image-link"></a>');
+        });
+
+        $(this).find('.image-link').each(function () {
+            $(this).attr('rel', 'article' + i);
+        });
     });
-  });
 
-  // Bootstrap table style
-  $('.article-entry table').each(function(i, table)  {
-    if ($(this).parent().hasClass('table-responsive')) return;
-    $(this).addClass('table');
-    $(this).wrap('<div class="table-responsive"></div>');
-  });
+    // Bootstrap table style
+    $('.article-entry table').each(function (i, table) {
+        if ($(this).parent().hasClass('table-responsive')) return;
+        $(this).addClass('table');
+        $(this).wrap('<div class="table-responsive"></div>');
+    });
 
-  // Lightbox plugin
-  if ($.fancybox){
-    $('.image-link').fancybox();
-  }
+    // Lightbox plugin
+    if ($.fancybox) {
+        $('.image-link').fancybox();
+    }
+
+    //has click ad
+    var hasClick = readCookie('has_click');
+    if (!hasClick) {
+        $("#myModal").show()
+            .click(function () {
+                createCookie('has_click', true, 1);
+                $("#myModal").hide();
+            });
+    }
 
 })(jQuery);
